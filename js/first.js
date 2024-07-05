70ae3cbcba24f7c07f0fe157c838d4c69f6a1c1a
mouis666666 <moaz666666@outlook.com>
2024-07-05 03:59:59 +0300

    tree

---
+++ js/first.js
@@ -0,0 +1,46 @@
+// window.alert("hello first")
+// document.getElementById("demo").innerHTML = "hello new js";
+// console.log("hello js from consol")
+
+// 0000000000000000000000000000000000000000000000000
+
+
+
+// var moaz_ryhan = 22;
+// console.log(moaz_ryhan)
+
+
+// 0000000000000000000000000000000000000000000000000
+
+
+// console.log(typeof(null));
+
+// 00000000000000000000000 hard and no performant 00000000000000000000000000
+
+// var first_number =  window.prompt("enter first number");
+// var first_number =  Number(first_number);
+// var second_number = window.prompt("enter second number");
+// var second_number =  Number(second_number);
+// console.log(first_number+second_number);
+
+
+// 0000000000000000000000 easy and good performant 000000000000000000000000000
+
+// var first_number = Number( window.prompt("enter first number"));
+// var second_number = Number (window.prompt("enter second number"));
+// console.log(first_number+second_number);
+
+
+// 0000000000000000000000000000000000000000000000000
+
+
+
+
+
+
+
+//======================================== number 1==================================================
+// console.log( window.prompt("enter the number"))
+
+//======================================== number 2==================================================
+
